f82cddaa42114ddce1bb026d8bd2a6f42da1a459
Alana McPhail <alanamcphail@utmuser171-15.wireless.utoronto.ca>
2022-06-28 12:46:55 -0400

    RMoving the NAs of the sleep columns

--- DataCleaning.R
+++ DataCleaning.R
@@ -9,4 +9,9 @@ dat <- read.csv("project_data.csv")
 dat2 <- dat %>% 
   mutate(ESSBinary = ifelse(Epworth.Sleepiness.Scale > 10, 1, 0)) %>% 
   mutate(PSGQBinary = ifelse(Pittsburgh.Sleep.Quality.Index.Score > 5, 1, 0)) %>%
-  mutate(AISBinary = ifelse(Athens.Insomnia.Scale > 5, 1, 0))
+  mutate(AISBinary = ifelse(Athens.Insomnia.Scale > 5, 1, 0)) 
+
+# Removing all NAs in those columns 
+dat3 <- dat2 %>%
+  filter(!is.na(ESSBinary), !is.na(PSGQBinary), !is.na(AISBinary), !is.na(Berlin.Sleepiness.Scale))
+
