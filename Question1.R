558ce2b2139827626be72b307ea3b3990b88c03b
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-06-28 12:56:02 -0400

    Prevalence for all conditions

--- Question1.R
+++ Question1.R
@@ -1,3 +1,13 @@
 #Question1
 
+#Prevalence for ESSBinary
 dat3$ESSBinary %>% table() %>% prop.table()
+
+#Prevalence for PSGQBinary
+dat3$PSGQBinary %>% table() %>% prop.table()
+
+#Prevalence for AISBinary
+dat3$AISBinary %>% table() %>% prop.table()
+
+#Prevalence for AISBinary
+dat3$Berlin.Sleepiness.Scale %>% table() %>% prop.table()
