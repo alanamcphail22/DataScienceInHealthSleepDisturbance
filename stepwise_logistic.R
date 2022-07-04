60bc13f5003db7893a52e3636149a53b92311e29
Papiha Joharapurkar <papihajoharapurkar@Papihas-MacBook-Pro.local>
2022-07-04 11:51:27 -0400

    Added residuals

--- stepwise_logistic.R
+++ stepwise_logistic.R
@@ -1,12 +1,12 @@
-###Data Transformation 
-
-# Data cleaning
+# ###Data Transformation
+# 
+# # Data cleaning
 library(dplyr)
 library(car)
 dat <- read.csv("project_data.csv")
 
 # Making variables binary
-dat2 <- dat %>% 
+dat2 <- dat %>%
   mutate(ESSBinary = ifelse(Epworth.Sleepiness.Scale > 10, 1, 0)) %>%
   mutate(PSGQBinary = ifelse(Pittsburgh.Sleep.Quality.Index.Score > 5, 1, 0)) %>%
   mutate(AISBinary = ifelse(Athens.Insomnia.Scale > 5, 1, 0))
